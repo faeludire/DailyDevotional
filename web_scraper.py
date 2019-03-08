c0b21bfc3ae74c7ac4280f2bca675f643bb2de43
faeludire <faeludire@gmail.com>
2019-03-08 08:58:05 +0000

    Add Soundcloud link

--- web_scraper.py
+++ web_scraper.py
@@ -12,12 +12,15 @@ def devotional_content_retrieval():
     souped = BeautifulSoup(page.text, 'html.parser')
 
     devotional_url = souped.find_all(class_="article-body")[0].find_all('a', href=True)[0]['href']
+    sound_cloud_link = souped.find_all("iframe")[0]['src']
 
     devotional_url = website_url_base + devotional_url
 
     devotional_conclusion = extract_devotional_conclusion(devotional_url)
 
-    tweet_lines = [devotional_conclusion, devotional_url]
+    links = 'Find out more: ' + devotional_url + '\n' + 'Audio version: ' + sound_cloud_link
+    tweet_lines = [devotional_conclusion, links]
+
     return tweet_lines
 
 
